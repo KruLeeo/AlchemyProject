e427af813290eb33b30e7318cc50ea8ea95cfaaa
ILya <tkachduty@gmail.com>
2023-11-19 14:22:54 +0300

    Добавлены картинки и стартовые страницы персонажей

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,16 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/Edward', function(req, res, next) {
+  res.send("<h1>Страница Эдварда</h1>")
+  });
+  router.get('/Alphonse', function(req, res, next) {
+    res.send("<h1>Страница Альфонса</h1>")
+    });
+    router.get('/Winry', function(req, res, next) {
+      res.send("<h1>Страница Винри</h1>")
+      });
+      
+  
 
 module.exports = router;
