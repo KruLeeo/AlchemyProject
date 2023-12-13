b06e72eb50315d585ae8d6fd2fae6e815ffe23ad
ILya <tkachduty@gmail.com>
2023-12-13 10:57:57 +0300

    Логика пользователя

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const Alche = require("../models/alche").Alche
+const Alche = require("../models/alche").Alche;
+var User = require("./../models/user").User
 
 
 /* GET home page. */  
@@ -15,10 +16,29 @@ router.get('/logreg', function(req, res, next) {
   res.render('logreg',{title: 'Вход'});
   });
   /* POST login/registration page. */
-router.post('/logreg', function(req, res, next) {
-  var username = req.body.username
-  var password = req.body.password
-});
+  router.post('/logreg', async function(req, res, next) {
+    try {
+      var username = req.body.username;
+      var password = req.body.password;
+      var user = await User.findOne({ username: username });
+      if (user) {
+        if (user.checkPassword(password)) {
+          req.session.user = user._id;
+          res.redirect('/');
+        } else {
+          res.render('logreg', { title: 'Вход' });
+        }
+      } else {
+        var newUser = new User({ username: username, password: password });
+        var savedUser = await newUser.save();
+        req.session.user = savedUser._id;
+        res.redirect('/');
+      }
+    } catch (err) {
+      next(err);
+    }
+  });
+
 
   
 module.exports = router;
