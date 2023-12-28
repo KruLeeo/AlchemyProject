3b050096faf773dd72bf5825d10253093ee52065
ILya <tkachduty@gmail.com>
2023-12-28 22:19:46 +0300

    Заполнение меню из базы

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -1,12 +1,13 @@
+
 const Alche = require("./../models/alche").Alche;
+
 module.exports = async function(req, res, next) {
-    try {
-      res.locals.nav = [];
-      const result = await Alche.find({}, { _id: 0, title: 1, nick: 1 });
-      res.locals.nav = result;
-      next();
-      
-    } catch (err) {
-      throw err;
-    }
+  try {
+    res.locals.nav = [];
+    const result = await Alche.find(null, { _id: 0, title: 1, nick: 1 });
+    res.locals.nav = result;
+    next();
+  } catch (err) {
+    throw err;
+  }
 };
